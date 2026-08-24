7009ad1c58c5b287a84c656d902bbe2c9db96d05
najiya <jiyaaapk@gmail.com>
2026-08-24 16:21:57 +0530

    email updated

--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -12,12 +13,68 @@ import { FormsModule } from '@angular/forms';
 export class ContactComponent {
 
   submitted = false;
+  sending = false;
+  errorMessage = '';
 
-  onSubmit() {
-    this.submitted = true;
-    setTimeout(() => {
-      this.submitted = false;
-    }, 3000);
+  formData = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  };
+
+  async onSubmit() {
+
+    if (this.sending) {
+      return;
+    }
+
+    this.sending = true;
+    this.submitted = false;
+    this.errorMessage = '';
+
+    const templateParams = {
+      name: this.formData.name,
+      email: this.formData.email,
+      title: this.formData.subject,
+      message: this.formData.message,
+      time: new Date().toLocaleString()
+    };
+
+    try {
+
+      await emailjs.send(
+        'service_fiz0t2q',
+        'template_cejpqlh',
+        templateParams,
+        {
+          publicKey: '2ky_jQ8FZenBVZkYd'
+        }
+      );
+
+      this.submitted = true;
+
+      this.formData = {
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+      };
+
+      setTimeout(() => {
+        this.submitted = false;
+      }, 2000);
+
+    } catch (error) {
+
+      console.error('EmailJS Error:', error);
+
+      this.errorMessage =
+        'Message could not be sent. Please try again.';
+
+    } finally {
+
+      this.sending = false;
+    }
   }
-
 }
